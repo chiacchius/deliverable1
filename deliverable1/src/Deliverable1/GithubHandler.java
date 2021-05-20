56b5ac5a1d30f9cbfa52cf3ff2090290ae11d5a7
Matteo <matteoch99@gmail.com>
2021-05-20 15:51:42 +0200

    finished

---
+++ deliverable1/src/Deliverable1/GithubHandler.java
@@ -0,0 +1,88 @@
+package Deliverable1;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.errors.TransportException;
+
+
+public class GithubHandler {
+	
+
+
+	public static Git cloneProjectFromGitHub(String path, String projectName) throws InvalidRemoteException, TransportException, GitAPIException, IOException {
+		//get the project from github and copy it locally
+		
+		Git git;
+		
+		try {
+			git = Git.cloneRepository()
+				       .setURI("https://github.com/apache/" + projectName)
+				       .setDirectory(new File(path)) 
+				       .call();
+		} catch (JGitInternalException e) {
+			//e.printStackTrace();
+			git = Git.open(new File(path));
+			
+		}
+		
+		return git;
+		
+		
+	}
+	
+	
+	public static List<RevCommit> getAllCommits(Git git) throws NoHeadException, GitAPIException, IOException{
+		
+		List<RevCommit> commits = new ArrayList<>();
+		Iterable<RevCommit> log = git.log().all().call();
+
+        for (RevCommit commit : log) {
+        		
+                commits.add(commit);
+        }
+		return commits;
+        
+        //System.out.print(commits.toString() + "\n");
+		
+	}
+
+
+	public static LocalDateTime findFixDate(List<RevCommit> commits, String key) {
+		
+		Collections.reverse(commits);
+		LocalDateTime dateTime = null;
+		
+		for (int i=0; i<commits.size(); i++) {
+			RevCommit commit = commits.get(i);
+			
+			if (commit.getFullMessage().contains(key+"\n")) {
+				
+				dateTime = Instant.ofEpochSecond(commit.getCommitTime()).atZone(ZoneId.of("UTC")).toLocalDateTime();
+				
+				//System.err.print("\nKey: " + ticket.getTicketKey() + "\n" + commit.getFullMessage() +"\n\n");
+				//System.out.println("ticket: " + ticket.getTicketKey() + "------>commit: " + commit.getFullMessage());
+				
+			}
+			
+		}
+		return dateTime;
+		
+		
+	}
+	
+	
+
+}
---
+++ deliverable1/src/Deliverable1/Handler.java
@@ -0,0 +1,101 @@
+package Deliverable1;
+
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Handler {
+	
+	
+	private static String readAll(Reader rd) throws IOException {
+	      StringBuilder sb = new StringBuilder();
+	      int cp;
+	      while ((cp = rd.read()) != -1) {
+	         sb.append((char) cp);
+	      }
+	      return sb.toString();
+	   }
+
+	public static JSONArray readJsonArrayFromUrl(String url) throws IOException, JSONException {
+	    InputStream is = new URL(url).openStream();
+	    try {
+	       BufferedReader rd = new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8));
+	       String jsonText = readAll(rd);
+	       JSONArray json = new JSONArray(jsonText);
+	       return json;
+	     } finally {
+	       is.close();
+	     }
+	 }
+	
+	 public static JSONObject readJsonFromUrl(String url) throws IOException, JSONException {
+	    InputStream is = new URL(url).openStream();
+	    try {
+	       BufferedReader rd = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
+	       String jsonText = readAll(rd);
+
+	       return new JSONObject(jsonText);
+	     } finally {
+	       is.close();
+	     }
+	 }
+	 
+	 
+	 
+	 public static void writeCsv(Map<String, LocalDateTime> resolutions, List<String> tickets) throws IOException{
+		   
+		   Integer numTickets;
+		   Integer i;
+		   FileWriter fileWriter = null;
+		   try {
+			   fileWriter = new FileWriter( "ProcessControlChart.csv"); //Name of CSV for output 
+		   }catch (Exception e) {
+			   //logger.info("Error in fileWriter creation\n");
+	         e.printStackTrace();
+		   }
+		   
+		   try{
+			   
+			 fileWriter.append("Ticket; Resolution Date");
+	         fileWriter.append("\n");
+	         numTickets = resolutions.size();
+	         for ( i = 0; i < numTickets; i++) {
+	             fileWriter.append(tickets.get(i));
+	             fileWriter.append(";");
+	             fileWriter.append(resolutions.get(tickets.get(i)).toString().subSequence(0, 10));
+	             fileWriter.append("\n");
+	          }
+			   
+		   } catch(Exception e){
+			   MyLogger.getSingletonInstance().saveMess("[*] Error in csv writing");
+			   e.printStackTrace();
+		   }
+		   finally {
+			   try {
+	             fileWriter.flush();
+	             fileWriter.close();
+	          } catch (IOException e) {
+	             //logger.info("Error while flushing/closing fileWriter !!!");
+	             e.printStackTrace();
+	          }
+		   }
+		
+		
+		
+	
+	 }
+}
+
