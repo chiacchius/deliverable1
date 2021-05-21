fd8c51fc0a018020ab9ac8880228ee7cbc7171b4
Matteo <matteoch99@gmail.com>
2021-05-21 15:12:12 +0200

    finish

---
+++ deliverable1/src/project/GithubHandler.java
@@ -0,0 +1,76 @@
+package project;
+
+import java.io.File;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.revwalk.RevCommit;
+
+public class GithubHandler {
+
+	private GithubHandler() {
+	}
+
+	public static Git cloneProjectFromGitHub(String path, String projectName) throws GitAPIException, IOException {
+		// get the project from github and copy it locally
+
+		Git git;
+
+		try {
+			git = Git.cloneRepository().setURI("https://github.com/apache/" + projectName).setDirectory(new File(path))
+					.call();
+		} catch (JGitInternalException e) {
+			git = Git.open(new File(path));
+
+		}
+
+		return git;
+
+	}
+
+	public static List<RevCommit> getAllCommits(Git git) throws GitAPIException, IOException {
+
+		List<RevCommit> commits = new ArrayList<>();
+		Iterable<RevCommit> log = git.log().all().call();
+
+		for (RevCommit commit : log) {
+			
+			MyLogger.getSingletonInstance().saveMess("######## Commit: " + commit.getFullMessage());
+			commits.add(commit);
+		}
+		return commits;
+
+
+	}
+
+	public static LocalDateTime findFixDate(List<RevCommit> commits, String key) throws SecurityException, IOException {
+
+		Collections.reverse(commits);
+		LocalDateTime dateTime = null;
+
+		for (int i = 0; i < commits.size(); i++) {
+			RevCommit commit = commits.get(i);
+
+			if (commit.getFullMessage().contains(key + "\n")) {
+
+				dateTime = Instant.ofEpochSecond(commit.getCommitTime()).atZone(ZoneId.of("UTC")).toLocalDateTime();
+
+				MyLogger.getSingletonInstance().saveMess("[*] Found commit related to ticket: " + key);
+
+			}
+
+		}
+		
+		return dateTime;
+
+	}
+
+}
--- deliverable1/src/project/MyLogger.java
+++ deliverable1/src/project/MyLogger.java
@@ -1,33 +1,32 @@
-package Deliverable1;
+package project;
 
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.SimpleFormatter;
 import java.util.logging.Logger;
 
-
 public class MyLogger {
-	
+
 	private static final String PATH = "./logFile.log";
-	
+
 	private static MyLogger instance = null;
-	private Logger logger = Logger.getLogger( "myLogger" );
+	private Logger logger = Logger.getLogger("myLogger");
 	private FileHandler fh;
-	
+
 	private MyLogger() throws SecurityException, IOException {
-		this.fh = new FileHandler( PATH );
+		this.fh = new FileHandler(PATH);
 		this.logger.addHandler(fh);
-		SimpleFormatter formatter = new SimpleFormatter();  
-        fh.setFormatter(formatter);
+		SimpleFormatter formatter = new SimpleFormatter();
+		fh.setFormatter(formatter);
 	}
-	
+
 	public static MyLogger getSingletonInstance() throws SecurityException, IOException {
-		if( instance == null )
+		if (instance == null)
 			instance = new MyLogger();
 		return instance;
 	}
-	
-	public void saveMess( String message ) {
+
+	public void saveMess(String message) {
 		this.logger.info(message);
 	}
 }
